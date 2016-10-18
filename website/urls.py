ddce159d851a2b74121de690898fd781ed97d642
rohitrango <rohit.rango@gmail.com>
2016-10-18 18:03:21 +0530

    venv3: Use this virtual env for all Django purposes

--- website/urls.py
+++ website/urls.py
@@ -1,13 +1,14 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
-    url(r'^$', views.singnin, name='signin'),
+    url(r'^$', views.signin, name='signin'),
     url(r'^home', views.home, name='home'),
-    url(r'^logout', view.logout, name='logout'),
-    url(r'^courses$', view.courses, name='view_course'),
-    url(r'^courses/add', view.add_courses, name='add_course'),
-    url(r'^assignments$', view.assigns, name='view_course'),
-    url(r'^assignments/add', view.add_assigns, name='add_course'),
-    url(r'^courses/(?P<pk>\S+)/(?P<year>\S+)/(?P<sem>\S+)', view.course_detail, name='c_detail')
+    url(r'^logout', views.logout, name='logout'),
+    url(r'^courses$', views.courses, name='view_course'),
+    url(r'^courses/add', views.add_courses, name='add_course'),
+    url(r'^assignments$', views.assigns, name='view_course'),
+    url(r'^assignments/add', views.add_assigns, name='add_course'),
+    url(r'^courses/(?P<pk>\S+)/(?P<year>\S+)/(?P<sem>\S+)', views.course_detail, name='c_detail')
 ]
--- website/views.py
+++ website/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def signin(request):
+	return HttpResponse('Sign in page!')
+
+def home(request):
+	return HttpResponse('Sign in page!')
+
+def logout(request):
+	return HttpResponse('Sign in page!')
+
+def courses(request):
+	return HttpResponse('Sign in page!')
+
+def add_courses(request):
+	return HttpResponse('Sign in page!')
+
+def assigns(request):
+	return HttpResponse('Sign in page!')
+
+def add_assigns(request):
+	return HttpResponse('Sign in page!')
+
+def course_detail(request,pk,year,sem):
+	return HttpResponse('Sign in page!')
