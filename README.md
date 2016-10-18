c74ba60fbccdc007dff010afc59f9b25b0875aa7
rohitrango <rohit.rango@gmail.com>
2016-10-18 18:08:54 +0530

    venv3

---
+++ README.md
@@ -0,0 +1,5 @@
+## Feed'er Website
+
+Parts - 
+feeder/ - project name
+website/ - app name (basicall the website we will be working on)
---
+++ venv3/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/rohitrango/CSE_IITB/Sem_3/CS251/Feed-er-Website/venv3/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
