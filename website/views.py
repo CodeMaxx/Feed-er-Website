d0d8b2a4ac1555da3c7056ca7c8abc80442c93d6
rohitrango <rohit.rango@gmail.com>
2016-10-22 16:19:36 +0530

    login and signup pages made

--- website/views.py
+++ website/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def signin(request):
-	return HttpResponse('Sign in page!')
+	return render(request, "signin.html")
 
 def home(request):
 	return HttpResponse('Sign in page!')
