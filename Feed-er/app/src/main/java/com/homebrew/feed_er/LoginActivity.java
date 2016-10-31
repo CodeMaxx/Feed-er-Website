5b8e18d426b370a052d77e417716516e4cff32c0
Meet <meet11061997@gmail.com>
2016-10-31 23:00:32 +0530

    autologin on restart

--- Feed-er/app/src/main/java/com/homebrew/feed_er/LoginActivity.java
+++ Feed-er/app/src/main/java/com/homebrew/feed_er/LoginActivity.java
@@ -4,7 +4,9 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.app.FragmentManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -95,6 +97,14 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        String token = sharedPref.getString("token","-1");
+        if(!token.equals("-1")){
+            Intent intent = new Intent(getApplicationContext(),HomeActivity.class);
+            intent.putExtra("token",token);
+            startActivity(intent);
+        }
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
@@ -252,6 +262,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                                 textView.setVisibility(View.VISIBLE);
                             }
                             else{
+                                SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+                                SharedPreferences.Editor editor = sharedPref.edit();
+                                editor.putString("token", responseString);
+                                editor.commit();
                                 Intent intent = new Intent(getApplicationContext(),HomeActivity.class);
                                 intent.putExtra("token",responseString);
                                 startActivity(intent);
